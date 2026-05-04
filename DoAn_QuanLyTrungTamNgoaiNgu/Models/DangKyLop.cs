90acd36fccae108bb7c0257fc00e11c8fdc9a9de
Johan Vx <tohongvu10a5@gmail.com>
2026-05-04 17:44:14 +0700

    feat(view): add user control DangKyLop

---
+++ DoAn_QuanLyTrungTamNgoaiNgu/Models/DangKyLop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_QuanLyTrungTamNgoaiNgu.Models
+{
+    public class DangKyLop
+    {
+        public string MaHV { get; set; }
+        public string MaLop { get; set; }
+
+        public string HoTenHV { get; set; }
+        public string TenLop { get; set; }
+        public DateTime NgayDK { get; set; }
+        public decimal HocPhi { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
---
+++ DoAn_QuanLyTrungTamNgoaiNgu/Views/DangKy/UC_DanhSachDangKy.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoAn_QuanLyTrungTamNgoaiNgu.Views.DangKy
+{
+    /// <summary>
+    /// Interaction logic for UC_DanhSachDangKy.xaml
+    /// </summary>
+    public partial class UC_DanhSachDangKy : UserControl
+    {
+        public UC_DanhSachDangKy()
+        {
+            InitializeComponent();
+        }
+    }
+}
