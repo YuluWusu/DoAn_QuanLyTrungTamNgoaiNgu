a9eddd5f5edc95371da0567fda28407a22ff358e
Johan Vx <tohongvu10a5@gmail.com>
2026-05-23 15:19:59 +0700

    feat(home): add navigation to register and tuition
    - Add command to navigate to RegisterViewModel
    - Add command to navigate to TuitionViewModel

--- DoAn_QuanLyTrungTamNgoaiNgu/ViewModels/VM_TrangChu.cs
+++ DoAn_QuanLyTrungTamNgoaiNgu/ViewModels/VM_TrangChu.cs
@@ -1,11 +1,7 @@
 ﻿using DoAn_QuanLyTrungTamNgoaiNgu.Helpers;
 using DoAn_QuanLyTrungTamNgoaiNgu.Models;
 using DoAn_QuanLyTrungTamNgoaiNgu.Views;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -77,6 +73,40 @@ namespace DoAn_QuanLyTrungTamNgoaiNgu.ViewModels
             data = new QL_TRUNGTAM_TIENGANH();
             NoiDungHienTai = new UC_TrangChu();
             NavTrangChu = new RelayCommand<object>((p) => true, (p) => { NoiDungHienTai = new UC_TrangChu(); });
+
+            NavTrangChu = new RelayCommand<object>((p) => true, (p) => { NoiDungHienTai = new UC_TrangChu(); });
+
+            // 3. Nút Đăng ký lớp
+            NavDangKyLop = new RelayCommand<object>((p) => true, (p) => 
+            { 
+                var vmDangKy = new VM_DangKy();
+        
+                // Bắt sự kiện khi bấm nút "+ THÊM ĐĂNG KÝ" trong UC_DanhSachDangKy
+                vmDangKy.OnRequestAddRegistration = () => 
+                {
+                    var vmDangKyMoi = new VM_DangKyMoi();
+                    // Bắt sự kiện khi bấm nút "Quay lại" hoặc "Hủy" trong form thêm mới
+                    vmDangKyMoi.RequestNavigateBack = () => NoiDungHienTai = vmDangKy;
+                    NoiDungHienTai = vmDangKyMoi;
+                };
+        
+                NoiDungHienTai = vmDangKy; 
+            });
+            NavHocPhi = new RelayCommand<object>((p) => true, (p) => 
+            { 
+                var vmDanhSachHocPhi = new VM_DanhSachHocPhi();
+        
+                // Bắt sự kiện khi bấm nút "+ THÊM KHÓA HỌC" trong UC_DanhSachHocPhi
+                vmDanhSachHocPhi.OnRequestAddKhoaHoc = () =>
+                {
+                    var vmThemHocPhi = new VM_ThemHocPhi();
+                    // Bắt sự kiện khi bấm nút "Quay lại" trong form thêm khóa học
+                    vmThemHocPhi.RequestNavigateBack = () => NoiDungHienTai = vmDanhSachHocPhi;
+                    NoiDungHienTai = vmThemHocPhi; 
+                };
+        
+                NoiDungHienTai = vmDanhSachHocPhi; 
+            });
         }
         public void LoadThongTinNguoiDung(string maTK)
         {
