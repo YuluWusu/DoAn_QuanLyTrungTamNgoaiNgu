50a1b533a31b96fbea5af3e3bd7cca5d0779e67d
Phi Long <yuluwusu@gmail.com>
2026-05-29 16:07:01 +0700

    Add files via upload

---
+++ DoAn_QuanLyTrungTamNgoaiNgu/Helpers/UserSession.cs
@@ -0,0 +1,11 @@
+namespace DoAn_QuanLyTrungTamNgoaiNgu.Helpers
+{
+    public static class UserSession
+    {
+        public static string MaTK { get; set; }
+        public static string HoTen { get; set; }
+        public static string VaiTro { get; set; }
+        public static string MaNV { get; set; }
+        public static string MaGV { get; set; }
+    }
+}
